340e0e6c2d82ceffe1feaca512b029f63d2ca586
Lincoln <lincoln@vulpes.com>
2024-01-31 20:00:34 -0500

    Absolute encoders & CAN fixes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,35 +25,45 @@ public class Constants {
     public class SwerveConstants {
 ;
         //DON'T KNOW THESE
-        public static double turningP = 0;
+        public static double turningP = 0.5;
         public static double turningI = 0;
         public static double turningD = 0;
-        public static double driveP = 0;
+        public static double driveP = 0.5;
         public static double driveI = 0;
         public static double driveD = 0;
 
         public static double trackWidth = 0.5588;
         public static double wheelBase = 0.4826;
 
-        
-        //goof
-        public static double wheelRadius = 0.05;
+        public static double wheelRadius = 0.0508;
+        public static double driveGearRatio = 6.75;
+        public static double turningGearRatio = 150.0/7;
 
         public static Translation2d frontLeftPosition = new Translation2d(trackWidth/2,wheelBase/2);
         public static Translation2d frontRightPosition = new Translation2d(-trackWidth/2,wheelBase/2);
         public static Translation2d backLeftPosition = new Translation2d(trackWidth/2,-wheelBase/2);
         public static Translation2d backRightPosition = new Translation2d(-trackWidth/2,-wheelBase/2);
 
-        public static int frontLeftDriveMotor = 0;
-        public static int frontLeftTurningMotor = 0;
+        public static int frontLeftDriveMotor = 2;
+        public static int frontLeftTurningMotor = 1;
 
-        public static int frontRightDriveMotor = 0;
-        public static int frontRightTurningMotor = 0;
+        public static int frontRightDriveMotor = 4;
+        public static int frontRightTurningMotor = 3;
 
-        public static int backRightDriveMotor = 0;
-        public static int backRightTurningMotor = 0;
+        public static int backRightDriveMotor = 6;
+        public static int backRightTurningMotor = 5;
 
-        public static int backLeftDriveMotor = 0;
-        public static int backLeftTurningMotor = 0;
+        public static int backLeftDriveMotor = 8;
+        public static int backLeftTurningMotor = 7;
+
+        public static int backLeftAbsoluteEncoder = 53;
+        public static int backRightAbsoluteEncoder = 50;
+        public static int frontLeftAbsoluteEncoder = 51;
+        public static int frontRightAbsoluteEncoder = 52;
+
+        public static int backLeftAbsoluteEncoderOffset = 0;
+        public static int backRightAbsoluteEncoderOffset = 0;
+        public static int frontLeftAbsoluteEncoderOffset = 0;
+        public static int frontRightAbsoluteEncoderOffset = 0;
     }
 }
